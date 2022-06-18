4169e48c2687431ee47de2b9fec933aa62849902
fisik-yum <govind.sriram@aol.com>
2022-06-17 20:31:50 -0700

    improved on modularity.

--- main.go
+++ main.go
@@ -17,43 +17,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"gocomi/gocomics"
 )
 
 func main() {
 	fmt.Println("gocomi version 2, fisik_yum\npress enter to select default values")
-	var sKey string //will store id later
-	selec := -1
-	fmt.Print("\nEnter Comic Strip Name:")
-	fmt.Scanf("%s", &sKey)
-	hList := search(sKey)
-	fmt.Printf("\nFound %d results\n\n", len(hList))
-	for x := range hList {
-		fmt.Printf("[%d] %s\n", x, hList[x].name)
-	}
-	for selec < 0 || selec >= len(hList) {
-		fmt.Printf("\nSelect entry [0-%d]:", len(hList)-1)
-		fmt.Scanf("%d\n", &selec)
-	}
-
-	sKey = hList[selec].id[1:] //shamelessy reusing variables
-	var startDate time.Time = startDate(sKey)
-	var dKey string
-
-	fmt.Printf("Enter scrape start date[yyyy/mm/dd][default=%d/%d/%d]:", startDate.Year(), startDate.Month(), startDate.Day())
-	fmt.Scanf("%s", &dKey)
-	if dKey != "" {
-		startDate = parsedate(dKey)
-	}
-	var endDate time.Time
-	fmt.Printf("Enter scrape start date[yyyy/mm/dd][default=%d/%d/%d]:", time.Now().Year(), time.Now().Month(), time.Now().Day())
-	fmt.Scanf("%s", &dKey)
-
-	if dKey != "" {
-		endDate = parsedate(dKey)
-	} else {
-		endDate = time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.Now().Location())
-	}
-	dlroutine(sKey, startDate, endDate)
-	fmt.Println("Operation Complete!")
+	gocomics.START()
 }
